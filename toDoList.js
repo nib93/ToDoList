d17b332afcde392e285b6b11dc7119c841349400
Neeti <neeti.i.bhatt@gmail.com>
2023-08-20 02:06:58 -0400

    adding logic for todo list still local storage logic is remaining

---
+++ toDoList.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+const form=document.getElementById("toDoForm");//Fetch form
+const task=document.getElementById("NewTask");//fetch unorder list of task
+
+//add task when submit button is clicked
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    //create new list element and store the task collected by form into it.
+    let newTask=document.createElement("li");
+    newTask.innerText=document.getElementById("ToDoTask").value;
+    newTask.style.color="green";
+
+    //Create close button
+    let close=document.createElement("button");
+    close.innerText="X";
+    
+
+    //add list element to HTML content
+    task.appendChild(newTask);
+    //add close button to task list
+    newTask.appendChild(close);
+
+});
+
+task.addEventListener("click",function(event){
+    const tageNameToDo=event.target.tagName.toLowerCase();
+
+    //if tageName is butten then remove the task
+    if(tageNameToDo==="button")
+        {
+            event.target.parentNode.remove();
+        }
+    else if(tageNameToDo==="li")
+    {
+        event.target.style.textDecoration = "line-through";
+        event.target.style.color="red";
+    }
+});
+});
+
---
+++ toDoListLocalStorage.js
@@ -0,0 +1,5 @@
+const form=document.getElementById("toDoForm");//Fetch form
+const task=document.getElementById("NewTask");//fetch unorder list of task
+
+
+//Add data into local storage
